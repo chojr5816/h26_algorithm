9bc24a573fa3cc66287d3d4b2bc918473f4e7cd0
chojr5816 <chojr3312@gmail.com>
2026-04-01 17:13:41 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 616 ms, Memory: 67004 KB -BaekjoonHub

--- Python/백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ Python/백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,42 +1,32 @@
+from collections import deque
 import sys
-
-# 1. 재귀 한도 설정 (DFS 사용 시 필수)
 sys.setrecursionlimit(10**6)
-
-# 2. 빠른 입력 함수 설정
-# input() 대신 sys.stdin.readline을 사용하면 읽기 속도가 수십 배 빠릅니다.
 input = sys.stdin.readline
 
-def dfs(v):
-    visited[v] = True
-    for i in graph[v]:
-        if not visited[i]:
-            dfs(i)
+# bfs 함수
+def bfs(graph, start, visited):
+    queue = deque([start])
+    visited[start] = True
 
-# N: 정점 개수, M: 간선 개수
-try:
-    line = input().split()
-    if not line:
-        exit()
-    n, m = map(int, line)
+    while queue:
+        v = queue.popleft()
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
 
-    # 인접 리스트 생성
-    graph = [[] for _ in range(n + 1)]
-    for _ in range(m):
-        u, v = map(int, input().split())
-        graph[u].append(v)
-        graph[v].append(u)
+n, m = map(int, input().split()) # 정점의 개수, 간선의 개수
+graph = [[] for _ in range(n+1)]
+for i in range(m):
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
 
-    visited = [False] * (n + 1)
-    count = 0
+count = 0 # 연결 노드의 수
+visited = [False] * (n+1)
+for i in range(1, n+1):
+    if not visited[i]:
+        bfs(graph, i, visited) # bfs 한 번 끝날 때마다 count+1
+        count += 1
 
-    # 연결 요소 탐색
-    for i in range(1, n + 1):
-        if not visited[i]:
-            dfs(i)
-            count += 1
-
-    print(count)
-
-except EOFError:
-    pass
+print(count)
