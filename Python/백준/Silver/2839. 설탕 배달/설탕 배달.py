d2ef05f84bd55619d397d521a07d4ac39b67f84f
chojr5816 <chojr3312@gmail.com>
2026-04-03 09:57:21 +0900

    [Silver IV] Title: 설탕 배달, Time: 296 ms, Memory: 32544 KB -BaekjoonHub

--- Python/백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ Python/백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,16 +1,21 @@
 N = int (input())
-num_bag = 0
 
-while True:
-    if N % 5 == 0:
-        num_bag += N // 5
-        break
-    
-    if N < 3:
-        num_bag = -1
-        break
+# 자료 구조 정의
+# 5kg, 3kg의 최대 봉지수
+max_5kg = N // 5
+max_3kg = N // 3
+min_bag = max_5kg + max_3kg
+is_found = False
 
-    N -= 3
-    num_bag += 1
-
-print(num_bag)
+# 알고리즘
+for num_5kg in range(max_5kg + 1):
+    for num_3kg in range(max_3kg + 1):
+        if num_5kg * 5 + num_3kg * 3 == N:
+            is_found = True
+            cur_bag = num_5kg + num_3kg
+            if min_bag > cur_bag:
+                min_bag = cur_bag
+if is_found: # N kg이 되는 봉지 수가 없으면 
+    print (min_bag)
+else:
+    print(-1)
